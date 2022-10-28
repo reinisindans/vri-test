bab8a2d2376598b4c6229aceb32e2d38bbc17055
Reinis Indāns <reinis.i@inbox.lv>
2022-10-28 16:02:05 +0300

    Added layer Controls

--- src/components/layerControl.js
+++ src/components/layerControl.js
@@ -1,22 +1,30 @@
 import React, { useState, useEffect } from "react";
-
+import { Switch } from "@mui/material";
 import "../css/layerControl.css";
 
 const LayerControl = (props) => {
-
-    const layers = ["heloo", "Hahaha"]
     
+
+    const onButtonPress = (event) => {
+        let currentButtonState = event.target.checked
+        console.log(currentButtonState)
+        console.log(event.target.value);
+        var newLayerArray = props.layers.map(layer => layer.id == event.target.value ? { ...layer, show: currentButtonState } : layer);
+        console.log(newLayerArray);
+        props.handleLayers(newLayerArray);
+  }  
+
   return (
     <div className="layerControlContainer">
       <div className="layerControlHeader">
         <text>Layers</text>
       </div>
       <div className="layerControlBody">
-        {layers.map((layer) => {
+        {props.layers.map((layer) => {
           return (
-              <div className="layerControlEntry" key={layer}>
-                  
-              <text className="layerControlLayerName">{layer}</text>
+            <div className="layerControlEntry" key={layer.id}>
+                  <Switch checked={layer.show} onChange={onButtonPress } value={layer.id} />
+              <text className="layerControlLayerName">{layer.title}</text>
             </div>
           );
         })}
--- src/components/map.js
+++ src/components/map.js
@@ -2,40 +2,52 @@ import React, { useState, useEffect } from "react";
 
 import { MapContainer, TileLayer, WMSTileLayer } from "react-leaflet";
 
-import { workspaceName, layerPath, WMSPath, legendGraphicTemplate } from "../constants/geoserver";
+import {
+  workspaceName,
+  layerPath,
+  WMSPath,
+  legendGraphicTemplate,
+} from "../constants/geoserver";
 import Legend from "./legend";
 import LayerControl from "./layerControl";
 
 import "leaflet/dist/leaflet.css";
 
 import "../css/map.css";
-import { isLabelWithInternallyDisabledControl } from "@testing-library/user-event/dist/utils";
 
 const MainMap = (props) => {
   const cesisLocation = [57.31485, 25.27096];
   const [layers, setLayers] = useState([]);
   const [layerStyles, setLayerStyles] = useState([]);
 
-
   const parseLayers = (requestResult) => {
     let layersArray = [];
     const layerArray = requestResult.layers.layer;
     for (let index in layerArray) {
-      console.log("Layer name: ", layerArray[index].name);
-      let layerObject= {"name":layerArray[index].name, "show":true, "id":index}
+      let layerObject = {
+        name: layerArray[index].name,
+        title: layerArray[index].name.split("_")[0],
+        show: true,
+        id: index,
+      };
       layersArray.push(layerObject);
     }
     return layersArray;
   };
 
   const getLayerStyles = (layerNamesArray) => {
-    let layerStylesArray = layerNamesArray.map((layer)=>{return { layerName: layer.name.split("_")[0], legendGraphic: legendGraphicTemplate +layer.name};})
+    let layerStylesArray = layerNamesArray.map((layer) => {
+      return {
+        layerName: layer.name.split("_")[0],
+        legendGraphic: legendGraphicTemplate + layer.name,
+      };
+    });
     setLayerStyles(layerStylesArray);
-  }
+  };
 
   const handleLayers = (layerArray) => {
-    setLayers(layerArray)
-  }
+    setLayers(layerArray);
+  };
 
   const getAllLayerNames = () => {
     const options = {
@@ -49,34 +61,37 @@ const MainMap = (props) => {
   };
 
   const drawLayer = (layer) => {
-    console.log("Trying to draw layer ", layer)
-    return layer.show ? <WMSTileLayer
-              layers={workspaceName + ":" + layer.name}
-              url={WMSPath}
-              transparent={true}
-              format="image/png"
-              opacity={0.8}
-              key={layer.id}
-    />
-      : null
-  }
+    return layer.show ? (
+      <WMSTileLayer
+        layers={workspaceName + ":" + layer.name}
+        url={WMSPath}
+        transparent={true}
+        format="image/png"
+        opacity={0.8}
+        key={layer.id}
+      />
+    ) : null;
+  };
 
-  useEffect(() => { getAllLayerNames() }, []);
-  useEffect(() => { getLayerStyles(layers) }, [layers]);
+  useEffect(() => {
+    getAllLayerNames();
+  }, []);
+  useEffect(() => {
+    getLayerStyles(layers);
+  }, [layers]);
 
   console.log("parsing map");
   return (
     <div className={"mapdiv"}>
       <Legend layerStyles={layerStyles} />
-      <LayerControl layerNames={ layers} handleLayers={handleLayers} />
+      <LayerControl layers={layers} handleLayers={handleLayers} />
       <MapContainer center={cesisLocation} zoom={13} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         {layers.map((layer) => {
-          console.log("getting layer: ", layer.name)
-          return drawLayer(layer)
+          return drawLayer(layer);
         })}
       </MapContainer>
     </div>
