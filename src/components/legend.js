590330bdbc02cdf6ab3c5ae989d839e69fbc1b3d
Reinis Indāns <reinis.i@inbox.lv>
2022-11-02 11:36:43 +0200

    Remove legend container when empty

--- src/components/legend.js
+++ src/components/legend.js
@@ -3,7 +3,7 @@ import React from "react";
 import "../css/legend.css"
 
 const Legend = (props) => {
-  return (
+  return props.layerStyles.length>0 ? (
     <div className="legendContainer">
       <div className="legendHeader">
         <text>Legend</text>
@@ -12,14 +12,18 @@ const Legend = (props) => {
         {props.layerStyles.map((layer) => {
           return (
             <div className="layerLegendEntry" key={layer.layerName}>
-              <img className="legendLayerGraphic" src={layer.legendGraphic} alt="legend graphic" />
+              <img
+                className="legendLayerGraphic"
+                src={layer.legendGraphic}
+                alt="legend graphic"
+              />
               <text className="legendLayerText">{layer.layerName}</text>
             </div>
           );
         })}
       </div>
     </div>
-  );
+  ) : null
 };
 
 export default Legend;
